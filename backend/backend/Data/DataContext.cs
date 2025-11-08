fa8c4b2675a631885fa50b890824a03f6b1ab4d2
Rebin Qadir <rebin.s.q@gmail.com>
2025-11-07 22:15:18 -0500

    api: add AppDbContext, DI registration, and DB connection string

---
+++ backend/backend/Data/DataContext.cs
@@ -0,0 +1,16 @@
+﻿using backend.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace backend.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Company> Companies { get; set; }
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
--- backend/backend/Program.cs
+++ backend/backend/Program.cs
@@ -1,6 +1,13 @@
+using backend.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+// Registering DataContext
+builder.Services.AddDbContext<DataContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
