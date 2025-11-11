1562384cac2f807339a9402d78f57f1ca3f1a0a4
Rebin Qadir <rebin.s.q@gmail.com>
2025-11-11 10:20:16 -0500

    chore(app): update App, Main, and add global styles

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,25 @@
-function App() {
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import EmployeesPage from "./pages/EmployeesPage";
+import AddEmployeePage from "./pages/AddEmployeePage";
+import EditEmployeePage from "./pages/EditEmployeePage";
+import Toast from "./components/Toast";
+import { CompanyProvider } from "./contexts/CompanyContext";
+import { EmployeeProvider } from "./contexts/EmployeeContext";
 
-  return (     
-      <h1>Employee Contact Management System</h1>
-  )
+export default function App() {
+  return (
+    <BrowserRouter>
+      <CompanyProvider>
+        <EmployeeProvider>
+          <Toast />
+
+          <Routes>
+            <Route path="/" element={<EmployeesPage />} />
+            <Route path="/employees/add" element={<AddEmployeePage />} />
+            <Route path="/employees/edit/:id" element={<EditEmployeePage />} />
+          </Routes>
+        </EmployeeProvider>
+      </CompanyProvider>
+    </BrowserRouter>
+  );
 }
-
-export default App
